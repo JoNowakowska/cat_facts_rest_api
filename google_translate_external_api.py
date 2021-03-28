aa3afd29fc353175e1b51fa5cdca1987b56c8dfc
Joanna Nowakowska <joanna.k.nowakowska@gmail.com>
2021-03-28 12:43:46 +0200

    Connection to Google Cloud Translation API implemented in google_translate_external_api.py

---
+++ google_translate_external_api.py
@@ -0,0 +1,44 @@
+import os
+
+credential_path = "service_account.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+
+def explicit():
+    from google.cloud import storage
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client.from_service_account_json(
+        'service_account.json')
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    # print(buckets)
+
+
+def translate_text(target, text):
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    import six
+    from google.cloud import translate_v2 as translate
+
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target)
+
+    print(u"Text: {}".format(result["input"]))
+    print(u"Translation: {}".format(result["translatedText"]))
+    print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+
+explicit()
+translate_text("pl", "Hello")
